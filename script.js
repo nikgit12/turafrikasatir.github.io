154f2864ac875fc5e40b6bc173e065863714bb8b
nikgit12 <125936213+nikgit12@users.noreply.github.com>
2023-05-07 16:09:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function goToBooking() { 
+    window.location.assign("bookingform.html"); 
+} 
+function process() { 
+    var package = document.getElementById('package').value;  
+    var num_of_people = document.getElementById('num-of-people').value;  
+    var totalPayment = parseInt(package) * parseInt(num_of_people);  document.getElementById('total').innerHTML = totalPayment;  setTimeout(() => { 
+     alert('Thank you!'); 
+     window.location.assign('about.html'); 
+    }, 1000); 
+}
+   
